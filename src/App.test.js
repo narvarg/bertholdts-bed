b93419992ddb9d0502d1f5930b7ff8875cd0d2c1
Nar Varg <87720947+NarVarg@users.noreply.github.com>
2023-09-03 06:58:38 -0500

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders hello world text', () => {
   render(<App />);
   const screenText = screen.getByText("Hello World!");
   expect(screenText).toBeVisible();
