4e643c4ed0d346c7a47059facaa29dccdf705070
Tim Shepherd <timothyshepherd@studiomatrix.com>
2023-09-30 05:47:06 +0000

    Sep 30, 2023, 12:47 AM

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello,</h1>
-        <h1>Welcome to Bertholdt's Bed.</h1>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>Hello,</h1>
+				<h1>Welcome to Bertholdt's Bed.</h1>
+			</header>
+		</div>
+	);
 }
 
 export default App;
